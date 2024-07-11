a79c7b4b16e9ca1e5fb6cbe86fed5a0c27c7e49f
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-07-11 19:56:03 +0200

    Create a sample dataset to display something while working

---
+++ src/ExampleData.js
@@ -0,0 +1,27 @@
+const exampleData = {
+    personalInfo: {
+        firstName: "Rafael",
+        lastName: "Botella",
+        email: "oneBot@gmail.com",
+        phoneNumber: "+34 555 555 555",
+        city: "Alicante"
+    },
+
+    education: [
+        {
+            degree: "Web Developement and Professional Archaeology",
+            yearOfExpedition: "2024",
+            school: "University of Zumo de Naranja"
+        },
+    ],
+
+    experience: [
+        {
+            position: "Freak and Geek",
+            company: "Estrats Treballs d'Arqueologia",
+            time: "2022 - 2024" //To Do: Enable this to capture the cool date-type input
+        },
+    ]
+}
+
+export default exampleData
