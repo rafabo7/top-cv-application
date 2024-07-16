a0a1fd54b684c2a1a9d871b012e54f99b781ede9
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-07-16 12:38:29 +0200

    Create states for education and experience info Create open state for the  edu and exp sections Add function to toggle the open state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useId } from 'react'
 
 import { Interface } from './components/Interface'
 import { Output } from './components/Output'
@@ -7,19 +8,40 @@ import exampleData from './ExampleData'
 import './App.css'
 
 function App() {
+  //Data states
   const [personalInfo, setPersonalInfo] = useState(exampleData.personalInfo)
+  const [education, setEducation] = useState(exampleData.education)
+  const [experience, setExperience] = useState(exampleData.experience)
+
+  //State for one section open at a time
+  const [open, setOpen] = useState('education')
 
   function handlePersonalInfoChange (e) {
     const { key } = e.target.dataset
     const value = e.target.value
     setPersonalInfo({ ...personalInfo, [key]: value })
+  }
 
+  function toggleIsOpen(section) {
+    setOpen(section)
+
+  }
+
+  function handleSectionChange (e) {
+    const { section } = exampleData
+    
   }
 
 
   return (
     <main className='app' >
-    <Interface onChange={handlePersonalInfoChange} personalInfo={personalInfo} />
+    <Interface 
+      onChange={handlePersonalInfoChange} 
+      personalInfo={personalInfo}
+      education={education}
+      experience={experience} 
+      isOpen={open}
+      toggleIsOpen={toggleIsOpen} />
     <Output personalInfo={personalInfo} />
       
     </main>
