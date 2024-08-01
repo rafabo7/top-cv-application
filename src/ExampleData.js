3f0098c2c99fc1885b47a598eddc8ba9f0581baa
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-08-01 09:53:45 +0200

    Update sample info with my own cv

--- src/ExampleData.js
+++ src/ExampleData.js
@@ -1,3 +1,4 @@
+import uniqid from 'uniqid'
 
 const exampleData = {
     personalInfo: {
@@ -9,25 +10,66 @@ const exampleData = {
 
     education: [
         {
-            degree: "Web Developement and Professional Archaeology",
+            degree: "Degree in Web Application Developement",
+            year: "2024 - present",
+            school: "Centre de Estudis Especifics a Distancia",
+            city: "Valencia, Spain",
+            id: uniqid(),            
+        },
+        {
+            degree: "Javascript's Algorithms and Data Structures",
             year: "2024",
-            school: "University of Zumo de Naranja",
-            city: "Alicante",
-            id: '0',
-            
+            school: "freeCodeCamp",
+            city: "San Francisco, Californa",
+            id: uniqid(),            
+        },
+        {
+            degree: "CS50's Introduction to Programming with Python",
+            year: "2023 - 2024",
+            school: "Harvard University",
+            city: "Cambridge, Massachusetts",
+            id: uniqid(),            
+        },
+        {
+            degree: "Responsive Web Design",
+            year: "2022",
+            school: "freeCodeCamp",
+            city: "San Francisco, California",
+            id: uniqid(),            
+        },
+        {
+            degree: "Master in Professional Archaeology and Historical Heritage Management",
+            year: "2021",
+            school: "University of Alicante",
+            city: "Alicante, Spain",
+            id: uniqid(),            
         },
     ],
 
     experience: [
         {
-            position: "Freak and Geek",
+            position: "Archaelogist (yes, seriously)",
             company: "Estrats Treballs d'Arqueologia",
-            description: "I worked on so many things I just can´t remember all. The make the Mr. fix-it of the year (I was the only one around)",
-            city: "Zamora",
-            time: "2022 - 2024", //To Do: Enable this to capture the cool date-type input
-            id: 0,
-            
-
+            description: "Digging holes, watching over construction workes and producing technical documentation",
+            city: "Alicante, Spain",
+            time: "2022 - present", //To Do: Enable this to capture the cool date-type input
+            id: uniqid() 
+        },
+        {
+            position: "Co-founder and Content creator",
+            company: "Excapic Culturals Experiences",
+            description: "Writting and testing historical and touristical tours for our own application. Emphasising accessibility and playful learning",
+            city: "Alicante, Spain",
+            time: "2021 - present", //To Do: Enable this to capture the cool date-type input
+            id: uniqid() 
+        },
+        {
+            position: "Waiter",
+            company: "Jacapaca bar de tapas",
+            description: "Serving food and drink. Attending to customers in the bar that probably makes the best croquettes in town.",
+            city: "Alicante, Spain",
+            time: "2021 - present", //To Do: Enable this to capture the cool date-type input
+            id: uniqid() 
         },
     ]
 }
